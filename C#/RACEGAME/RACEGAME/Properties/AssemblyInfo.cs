28e11903eba395466734a20e12fb13b25447ea09
zroya <petervandervelde2@gmail.com>
2016-10-28 15:05:25 +0200

    Dont Let Me Down -- Mercy

--- C#/RACEGAME/RACEGAME/Properties/AssemblyInfo.cs
+++ C#/RACEGAME/RACEGAME/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Engine")]
+[assembly: AssemblyTitle("RACEGAME")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Engine")]
+[assembly: AssemblyProduct("RACEGAME")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("345d112a-123e-4261-bd79-1f40aedf9295")]
+[assembly: Guid("9756575b-25ce-48f5-98b6-69a2ae8078ee")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ C#/RACEGAME/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlCanvas = new System.Windows.Forms.Panel();
+            this.lblForce = new System.Windows.Forms.Label();
+            this.lblAngle = new System.Windows.Forms.Label();
+            this.tmrGameTicks = new System.Windows.Forms.Timer(this.components);
+            this.pnlCanvas.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlCanvas
+            // 
+            this.pnlCanvas.Controls.Add(this.lblForce);
+            this.pnlCanvas.Controls.Add(this.lblAngle);
+            this.pnlCanvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlCanvas.Location = new System.Drawing.Point(0, 0);
+            this.pnlCanvas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlCanvas.Name = "pnlCanvas";
+            this.pnlCanvas.Size = new System.Drawing.Size(1045, 690);
+            this.pnlCanvas.TabIndex = 0;
+            this.pnlCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlCanvas_Paint);
+            // 
+            // lblForce
+            // 
+            this.lblForce.AutoSize = true;
+            this.lblForce.Location = new System.Drawing.Point(961, 39);
+            this.lblForce.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblForce.Name = "lblForce";
+            this.lblForce.Size = new System.Drawing.Size(46, 17);
+            this.lblForce.TabIndex = 1;
+            this.lblForce.Text = "label1";
+            // 
+            // lblAngle
+            // 
+            this.lblAngle.AutoSize = true;
+            this.lblAngle.Location = new System.Drawing.Point(961, 11);
+            this.lblAngle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAngle.Name = "lblAngle";
+            this.lblAngle.Size = new System.Drawing.Size(46, 17);
+            this.lblAngle.TabIndex = 0;
+            this.lblAngle.Text = "label1";
+            // 
+            // tmrGameTicks
+            // 
+            this.tmrGameTicks.Enabled = true;
+            this.tmrGameTicks.Interval = 18;
+            this.tmrGameTicks.Tick += new System.EventHandler(this.tmrGameTicks_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApplication2.Properties.Resources.OpDeKaart;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1045, 690);
+            this.Controls.Add(this.pnlCanvas);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.pnlCanvas.ResumeLayout(false);
+            this.pnlCanvas.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlCanvas;
+        private System.Windows.Forms.Timer tmrGameTicks;
+        private System.Windows.Forms.Label lblAngle;
+        private System.Windows.Forms.Label lblForce;
+    }
+}
+
